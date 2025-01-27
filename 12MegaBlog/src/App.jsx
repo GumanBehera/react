9512765fe96508bc261bc2dcd54ceec646cf3d95
GumanBehera <guman.17917@sakec.ac.in>
2025-01-27 20:51:59 +0530

    Mega project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+console.log(import.meta.env.VITE_APPWRIRTE_URL)
+
+  return (
+    <>
+      MegaBlog project with appwrite
+    </>
+  )
+}
+
+export default App
